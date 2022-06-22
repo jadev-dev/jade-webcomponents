8eb43d82c9cb1f4a5424d3ef74ba7bdcd9a0118d
jadev-dev <me@jadev.dev>
2022-06-22 02:29:30 -0400

    more-details component and collection of resources

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# jade-webcomponents
+# web components
+## what
+over-engineered (and pretty useless) vanilla web components for
+developing skills with
+[custom elements](https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements),
+[shadow DOM](https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM), and
+[HTML templates / slots](https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_templates_and_slots).
+
+---
+## relevant resources
+* [web components - MDN](https://developer.mozilla.org/en-US/docs/Web/Web_Components)
+* [web components - web.dev](https://web.dev/web-components/)
+  - [overview](https://web.dev/custom-elements-v1/)
+  - [shadow DOM](https://web.dev/shadowdom-v1/)
+  - [best practices](https://web.dev/custom-elements-best-practices/)
+  - [examples](https://github.com/GoogleChromeLabs/howto-components)
+* [web components - MJT](https://javascript.info/web-components)
+  - [overview](https://javascript.info/webcomponents-intro)
+  - [custom elements](https://javascript.info/custom-elements)
+  - [shadow dom](https://javascript.info/shadow-dom)
+  - [template element](https://javascript.info/template-element)
+  - [slots](https://javascript.info/slots-composition)
+  - [shadow DOM styles](https://javascript.info/shadow-dom-style)
+  - [shadow DOM events](https://javascript.info/shadow-dom-events)
+* [how we use web components - web.dev](https://web.dev/how-we-build-webdev-and-use-web-components/#web-components)
+* [presentational and container components - dan abramov](https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0)
+
+---
+## examples and inspiration 
+* [material-webcomponents - DannyMoerkerke](https://github.com/DannyMoerkerke/material-webcomponents)
+* [ken-burns-carousel - Festify (TypeScript)](https://github.com/Festify/ken-burns-carousel)
+* [zoo-web-components](https://github.com/zooplus/zoo-web-components)
+* [lite-youtube - justinribeiro (TypeScript)](https://github.com/justinribeiro/lite-youtube)
+* [typewritten-text - Auroratide](https://github.com/Auroratide/typewritten-text)
+
+---
+## other useful things
+* [using import.meta.url to reference assets](https://modern-web.dev/guides/going-buildless/es-modules/#referencing-reusable-assets-with-importmetaurl)
---
+++ elements/more-details/more-details.js
@@ -0,0 +1,123 @@
+const template = document.createElement('template');
+template.innerHTML = /* html*/`
+<style>
+    :host {
+        display: flex;
+        font-family: var(--details-font, sans-serif);
+        flex-direction: column;
+    }
+</style>
+<slot name="text"></slot>
+<a href="javascript:void(0)" part="detail-toggle">more details...</a>
+<slot name="details"></slot>
+`;
+
+/** web component that offers optional details to the user */
+export class MoreDetails extends HTMLElement {
+  /** create component and declare members for slots */
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this._detailToggle = this.shadowRoot.querySelector('a');
+    this._textSlot = this.shadowRoot.querySelector('slot[name=text]');
+    this._detailsSlot = this.shadowRoot.querySelector('slot[name=details]');
+  }
+
+  /** hide or show elements based on the updated attribute */
+  attributeChangedCallback() {
+    this._updateHiddenState();
+    // since there are only two possible states, this logic isn't needed
+    // const value = this.hasAttribute('toggled');
+    // (() => value ? this._show() : this._hide())();
+  }
+
+  /** add event listeners when element is attached to DOM */
+  connectedCallback() {
+    this._detailToggle.addEventListener('click', () => this._onClick());
+    this._detailsSlot.addEventListener('slotchange', () => this._slotChange());
+  }
+
+  /** clean up and remove listeners */
+  disconnectedCallback() {
+    this._detailToggle.removeEventListener(
+        'click', () => this._onClick(),
+    );
+    this._detailsSlot.removeEventListener(
+        'slotchange', () => this._slotChange(),
+    );
+  }
+
+  /**
+   * return an array of all elements in details slot
+   * @return {Array} detailElements
+   */
+  _allDetails() {
+    return Array.from(this.querySelectorAll('*[slot=details]'));
+  }
+
+  /** automatically hide or show elements when slotted */
+  _slotChange() {
+    this._updateHiddenState();
+  }
+
+  /** swap toggled state */
+  _onClick() {
+    this.toggled = !(this.toggled);
+  }
+
+  /**
+   * reflect property state in attribute and vice-versa
+   * @param {Boolean} value
+   */
+  set toggled(value) {
+    value = Boolean(value);
+    if (value) {
+      this.setAttribute('toggled', '');
+    } else {
+      this.removeAttribute('toggled');
+    }
+  }
+
+  /**
+   * List of attributes that will fire attributeChangedCallback
+   * @return {Array} attributeList
+   */
+  static get observedAttributes() {
+    return ['toggled'];
+  }
+
+  /**
+   * Return toggled state
+   * @return {Boolean} toggled
+   */
+  get toggled() {
+    return this.hasAttribute('toggled');
+  }
+
+  /** set all detail slot elements to be visible and update text */
+  _show() {
+    const detailElements = this._allDetails();
+    detailElements.forEach((el) => el.hidden = false);
+    this._detailToggle.textContent = 'fewer details...';
+  }
+
+  /** set all detail slot elements to be hidden and update text */
+  _hide() {
+    const detailElements = this._allDetails();
+    detailElements.forEach((el) => el.hidden = true);
+    this._detailToggle.textContent = 'more details...';
+  }
+
+  /** update the hidden state of all detail slot elements */
+  _updateHiddenState() {
+    const detailElements = this._allDetails();
+    detailElements.forEach((el) => el.hidden = this.toggled ? false : true);
+    this._detailToggle.textContent = (
+      this.toggled ? 'fewer details...' : 'more details...'
+    );
+  }
+}
+
+customElements.define('more-details', MoreDetails);
